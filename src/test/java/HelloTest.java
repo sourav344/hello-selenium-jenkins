89d1f0a170a090fefdb8e7131765dcadb8bbf134
pinki3333 <pinkisusmita96@gmail.com>
2026-02-28 21:54:10 +0530

    initial commit

---
+++ src/test/java/HelloTest.java
@@ -0,0 +1,14 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class HelloTest {
+
+    @Test
+    public void openGoogle() {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.google.com");
+        System.out.println(driver.getTitle());
+        driver.quit();
+    }
+}
